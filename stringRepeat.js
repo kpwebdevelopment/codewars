88fdc133db908999ea3ecada6c2241e148603015
Kenneth Paige <77613987+kpwebdevelopment@users.noreply.github.com>
2025-05-16 16:34:52 -0400

    add solution for stringRepeat.js

---
+++ stringRepeat.js
@@ -0,0 +1,9 @@
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr (n, s) {
+  let str = '';
+  for (let i = 0; i < n; i++) {
+    str += s 
+  }
+  return str;
+}
